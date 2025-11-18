4c7033fd48de8b186f074435efd85de6ce6087f0
Zohaib Saeed <zohaibsaeed1701@gmail.com>
2025-11-18 20:12:23 +0000

    Get + Post + requirements.txt

--- app/app.py
+++ app/app.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI(title="Simple FastAPI Project")
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from app.py!"}
+data = {1: {"data": "This is first"}, 2: {"data": "This is second"}}
 
-@app.get("/hello_world")
-def get_user():
-    return {"message": "Hello Word"}
+@app.get("/test")
+def test():
+    return data
+
+@app.get("/test/{id}")
+def testing(id: int):
+    if id not in data:
+        raise HTTPException(status_code=401, detail="This id not found")
+    return data.get(id)
+
+@app.post("/test/{id}")
+def test(id: int):
+    if id not in data:
+        raise HTTPException(status_code=401, detail="This id not found")
+    return data.get(id)
