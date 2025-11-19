9fd3ffd0eb88dd6633b82ad49a448c5a04df47a4
Zohaib Saeed <zohaibsaeed1701@gmail.com>
2025-11-19 08:38:04 +0000

    Update app.py with file upload endpoint

--- app/app.py
+++ app/app.py
@@ -1,15 +1,13 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, UploadFile, File, Form
 from db import get_db
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 
 app = FastAPI(title="Professional FastAPI Project")
 
-
 @app.get("/")
 def home():
     return {"message": "FastAPI project is running successfully!"}
-
 
 @app.get("/test-db")
 def test_db_connection(db: Session = Depends(get_db)):
@@ -18,3 +16,11 @@ def test_db_connection(db: Session = Depends(get_db)):
         return {"status": "Database connected successfully!"}
     except Exception as e:
         return {"error": str(e)}
+
+@app.post("/update")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    pass
